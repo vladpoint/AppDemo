f8d861bd7ee02d1d2322b9867f5465876bd6e41d
Vladimir Medina <Vladimir Medina>
2017-09-06 23:12:57 -0500

    Add project files.

---
+++ App2/App2/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Label1.Text = "Dispositivos Móviles";
+        }
+    }
+}
